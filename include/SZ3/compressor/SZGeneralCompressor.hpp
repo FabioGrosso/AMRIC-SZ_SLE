5565323ed0ff114050fcd6e35a4923187ca7b3de
Daoce Wang - 358403 <daoce@darwin-fe1.lanl.gov>
2024-06-13 14:51:45 -0600

    rd access

--- include/SZ3/compressor/SZGeneralCompressor.hpp
+++ include/SZ3/compressor/SZGeneralCompressor.hpp
@@ -11,6 +11,7 @@
 #include "SZ3/utils/Timer.hpp"
 #include "SZ3/def.hpp"
 #include <cstring>
+#include <chrono>
 
 size_t arrSum(size_t *arr, size_t n) {
     size_t sum = 0; 
@@ -45,7 +46,7 @@ namespace SZ {
             size_t regCntSum = 0;
             std::vector<int> quant_inds;
             std::vector<int> meta_inds;
-            size_t bufferSize = 39305830;
+            size_t bufferSize = 393058300;
             uchar *buffer = new uchar[bufferSize];
             uchar *buffer_pos = buffer;
             uchar *buffer_temp = buffer;
@@ -66,7 +67,7 @@ namespace SZ {
                 } 
             }
             
-            //std::cout << "regCntSum: " << regCntSum << std::endl;
+            // std::cout << "regCntSum: " << regCntSum << std::endl;
 
             if (regCntSum) {
                 HuffmanEncoder<int> reg_huffman = HuffmanEncoder<int>();
@@ -88,6 +89,7 @@ namespace SZ {
             // buffer_temp = buffer_pos;
             encoder.save(buffer_pos);
             // std::cout << "datatree size: " << buffer_pos - buffer_temp << std::endl;
+            // std::cout << "buffer_pos: " << buffer_pos << std::endl;
 
             // buffer_temp = buffer_pos;
             encoder.encode(quant_inds, buffer_pos);
@@ -95,7 +97,7 @@ namespace SZ {
             encoder.postprocess_encode();
 
             assert(buffer_pos - buffer < bufferSize);
-            //std::cout << "real bufferSize: " << buffer_pos - buffer << std::endl;
+            // std::cout << "real bufferSize: " << buffer_pos - buffer << std::endl;
 
             uchar *lossless_data = lossless.compress(buffer, buffer_pos - buffer, compressed_size);
             lossless.postcompress_data(buffer);
@@ -122,6 +124,7 @@ namespace SZ {
             //std::cout << "frontend.get_num_elements()" << frontend.get_num_elements() << std::endl;
             size_t blkSize3D = blkSize*blkSize*blkSize;
             size_t blkNum = frontend.get_num_elements()/(blkSize3D);
+            // std::cout << "blkNum: " << blkNum << std::endl;
 
             auto compressed_data = lossless.decompress(cmpData, remaining_length);
             uchar const *compressed_data_pos = compressed_data;
@@ -157,16 +160,22 @@ namespace SZ {
             encoder.load(compressed_data_pos, remaining_length);
 
             timer.start();
-            auto quant_inds = encoder.decode(compressed_data_pos, frontend.get_num_elements());
-
+            auto startDequant= std::chrono::high_resolution_clock::now();
+            auto quant_inds = encoder.decode(compressed_data_pos, frontend.get_num_elements()/4);
+            encoder.postprocess_decode();
             // std::cout << "de quant size: " << quant_inds.size() << std::endl;
+            auto endDequant = std::chrono::high_resolution_clock::now();
+            double time_takenDequant = std::chrono::duration_cast<std::chrono::nanoseconds>(endDequant - startDequant).count() * 1e-9; // Convert nanoseconds to seconds
+            std::cout << "Time taken by Decode is : "  << std::fixed << std::setprecision(5) << time_takenDequant << " sec" << std::endl;
+            timer.stop("Decoder");
 
             count = 0;
 
             auto *decDataPos = decData;
             size_t tempInd = 0;
 
-            for (size_t i = 0; i < blkNum; ++i) {
+            auto startDepre= std::chrono::high_resolution_clock::now();
+            for (size_t i = 0; i < blkNum/4; ++i) {
                 if (regCntSum) {
                     std::vector<int> sub_reg {reg_vector.begin() + arrSum(regArr, count), reg_vector.begin() + arrSum(regArr, count + 1)};
                     frontend.load(ptr[count], remainArr[count], blkSize, blkSize, blkSize, regCnt, sub_reg, 1);
@@ -175,11 +184,36 @@ namespace SZ {
                 }
                 std::vector<int> sub_quant {quant_inds.begin() + tempInd, quant_inds.begin() + tempInd + blkSize3D};
                 tempInd += blkSize3D;
-                encoder.postprocess_decode();
+                // encoder.postprocess_decode();
                 frontend.decompress(sub_quant, decDataPos);
                 decDataPos += blkSize3D;
                 count++;
             }
+            auto endDepre = std::chrono::high_resolution_clock::now();
+            double time_takenDepre = std::chrono::duration_cast<std::chrono::nanoseconds>(endDepre - startDepre).count() * 1e-9; // Convert nanoseconds to seconds
+            std::cout << "Time taken by Depre is : "  << std::fixed << std::setprecision(5) << time_takenDepre << " sec" << std::endl;
+
+            // std::vector<int> sub_reg, sub_quant(blkSize3D);
+
+            // for (size_t i = 0; i < blkNum; ++i) {
+            //     if (regCntSum) {
+            //         int start = arrSum(regArr, count);
+            //         int end = arrSum(regArr, count + 1);
+            //         sub_reg.assign(reg_vector.begin() + start, reg_vector.begin() + end);
+            //         frontend.load(ptr[count], remainArr[count], blkSize, blkSize, blkSize, regCnt, sub_reg, 1);
+            //     } else {
+            //         frontend.load(ptr[count], remainArr[count], blkSize, blkSize, blkSize, regCnt, reg_vector, 1);
+            //     }
+                
+            //     std::copy(quant_inds.begin() + tempInd, quant_inds.begin() + tempInd + blkSize3D, sub_quant.begin());
+            //     tempInd += blkSize3D;
+
+            //     frontend.decompress(sub_quant, decDataPos);
+            //     decDataPos += blkSize3D;
+            //     count++;
+            // }
+
+            // std::cout << "down decomp" << std::endl;
 
             lossless.postdecompress_data(compressed_data);
             return decData;
